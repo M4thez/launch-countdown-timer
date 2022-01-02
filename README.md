946004a896e626d12d8780256e00d8cf22844221
M4thez <namemat@gmail.com>
2022-01-02 15:33:49 +0100

    Shrinking improvements

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Launch-countdown-timer - Desktop solution
 
+Live site: <https://m4thez.github.io/launch-countdown-timer/>
+
 Frontend Mentor challenge - <https://www.frontendmentor.io/challenges/launch-countdown-timer-N0XkGfyz->
 
 Flip-clock powered by PQINA - <https://pqina.nl/?ref=credits>
 
-Made by Mateusz Boboryko
+App made by Mateusz Boboryko
--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 function handleTickInit(tick) {
-  
   // format of due date is ISO8601
   // https://en.wikipedia.org/wiki/ISO_8601
-  
+
   // '2018-01-31T12:00:00'        to count down to the 31st of January 2018 at 12 o'clock
   // '2019'                       to count down to 2019
   // '2018-01-15T10:00:00+01:00'  to count down to the 15th of January 2018 at 10 o'clock in timezone GMT+1
-  
+
   // create the countdown counter
   const countDownDate = new Date().getFullYear() + 1;
   const counter = Tick.count.down(countDownDate.toString());
 
-  counter.onupdate = function(value) {
+  counter.onupdate = function (value) {
     tick.value = value;
   };
-
 }
